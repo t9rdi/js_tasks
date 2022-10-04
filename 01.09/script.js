4c34e398ea379eeec00f7e1f13d44d1db221dbf2
Arafat <turdievarafat77@gmail.com>
2022-10-04 20:16:46 +0600

    first:01.09

---
+++ 01.09/script.js
@@ -0,0 +1,17 @@
+const a = 17;
+const b = 10;
+const c = a - b;
+console.log(c);
+const d = 7;
+const result = c + d;
+console.log(result);
+
+
+var num = 10;
+num = ++num;
+num = num++;
+alert(num);
+
+var today   = new Date (10/2/2022)
+var now = today.toLocaleTimeString('en-US');
+console.log(now);
