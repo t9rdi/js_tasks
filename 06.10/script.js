3a57f4bfbaf7cf2c1dfdb8e384af04e80e8b8a1f
Arafat <turdievarafat77@gmail.com>
2022-10-08 19:58:01 +0600

    feat: 06.10

---
+++ 06.10/script.js
@@ -0,0 +1,30 @@
+let x = 7;
+if (x === 7){
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+
+let a = 1;
+let b = 3;
+if (a <= 1){
+    alert(1);
+} else if (b >= 3){
+    alert(3);
+} else{
+    alert('Результат вычитания 2');
+}
+
+const time = 46;
+if ( time >= 0 && time <= 14 ) {
+    alert('В первую четверть.');
+}
+if (time >= 15 && time <= 30 ) {
+    alert('Во вторую четверть.');
+}
+if ( time >= 31 && time <= 45 ) {
+    alert('В третью четверть.');
+}
+if ( time >= 46 && time <= 59 ) {
+    alert('В четвертую четверть.');
+}
