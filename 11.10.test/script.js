fdf24418cadf6173b34fe66715cae1c7b33d06cf
Arafat <turdievarafat77@gmail.com>
2022-10-13 20:20:02 +0600

    feat: 11.10.test

---
+++ 11.10.test/script.js
@@ -0,0 +1,19 @@
+var userSalaries = {
+    Ella: 350,
+    Sophia: 550,
+    Ellie: 300
+};
+console.log(userSalaries.Ella,userSalaries.Sophia);
+
+var x = {
+    a: 5,
+    b: 6,
+    c: 10
+};
+let result = x.a + x.b + x.c;
+console.log(result);
+
+
+let number = true;
+let date = (number === true) ? 3 : 5;
+console.log(date);
